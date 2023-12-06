6fe1a692b255d4a0e688ef89fb9f9e88cd6cad85
Divya R <kwrdivyaraj100@gmail.com>
2023-12-06 13:29:28 +0530

    Updated DrawerComp to include dynamic list items and styling

--- my-app/src/components/DrawerComp.jsx
+++ my-app/src/components/DrawerComp.jsx
@@ -2,24 +2,28 @@ import React, { useState } from 'react';
 import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 
-function DrawerComp() {
-    const [open, setOpen] =useState(false);
+function DrawerComp({ links }) {
+    const [open, setOpen] = useState(false);
     return (
         <>
-        <Drawer open={open} onClose={() => setOpen(false)}>
-        <List>
-            <ListItemButton>
-                <ListItemIcon>
-                    <ListItemText>
-                        Products
-                    </ListItemText>
-                </ListItemIcon>
-            </ListItemButton>
-        </List>
-        </Drawer>
-        <IconButton sx={{marginLeft: 'auto', color: "white"}} onClick={() => setOpen(!open)} >
-            <MenuRoundedIcon/>
-        </IconButton>
+            <Drawer PaperProps={{
+                sx: { backgroundColor: 'rgba(49, 49, 116, 1)' }
+            }} open={open} onClose={() => setOpen(false)}>
+                <List>
+                    {links.map((link, index) => (
+                        <ListItemButton key={index} divider>
+                            <ListItemIcon>
+                                <ListItemText sx={{ color: "white" }}>
+                                    {link}
+                                </ListItemText>
+                            </ListItemIcon>
+                        </ListItemButton>
+                    ))}
+                </List>
+            </Drawer>
+            <IconButton sx={{ marginLeft: 'auto', color: "white" }} onClick={() => setOpen(!open)} >
+                <MenuRoundedIcon />
+            </IconButton>
         </>
     )
 }

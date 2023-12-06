d761b9b1d938a5a31b64cf3e8f9f722c97a18fd5
Divya R <kwrdivyaraj100@gmail.com>
2023-12-06 12:41:44 +0530

    Creating DrawerComp component

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -5,10 +5,11 @@ import ShoppingCartCheckout from '@mui/icons-material/ShoppingCartCheckout';
 function Navbar({ links }) {
     const [value, setValue] = useState(0);
     return (
-        <AppBar sx={{backgroundImage: 'linear-gradient(90deg, rgba(100, 58, 58, 1) 2%, rgba(49, 49, 116, 1) 36%, rgba(105, 0, 161, 1) 73%, rgba(166, 69, 252, 1) 100%)'
+        <AppBar sx={{
+            backgroundImage: 'linear-gradient(90deg, rgba(180, 58, 58, 1) 2%, rgba(49, 49, 116, 1) 36%, rgba(105, 0, 161, 1) 73%, rgba(166, 69, 252, 1) 100%)'
         }}>
             <Toolbar>
-                <Grid container>
+                <Grid sx={{ placeItems: "center" }} container>
                     <Grid item xs={2}>
                         <Typography>
                             <ShoppingCartCheckout />
@@ -24,8 +25,8 @@ function Navbar({ links }) {
                     <Grid item xs={1} />
                     <Grid item xs={3}>
                         <Box display="flex">
-                            <Button sx={{marginLeft: 'auto'}} variant='contained'>Login</Button>
-                            <Button sx={{marginLeft: 1}} variant='contained'>SignUp</Button>
+                            <Button sx={{ marginLeft: 'auto', background: "rgba(180, 58, 58, 1)" }} variant='contained'>Login</Button>
+                            <Button sx={{ marginLeft: 1, background: "rgba(180, 58, 58, 1)" }} variant='contained'>SignUp</Button>
                         </Box>
                     </Grid>
                 </Grid>

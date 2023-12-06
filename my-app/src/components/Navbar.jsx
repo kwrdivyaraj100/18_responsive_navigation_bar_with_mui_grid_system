9329f5464036931f00188ed678ce3a2622579173
Divya R <kwrdivyaraj100@gmail.com>
2023-12-06 12:35:20 +0530

    Added linear gradient background to the component

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import ShoppingCartCheckout from '@mui/icons-material/ShoppingCartCheckout';
 function Navbar({ links }) {
     const [value, setValue] = useState(0);
     return (
-        <AppBar>
+        <AppBar sx={{backgroundImage: 'linear-gradient(90deg, rgba(100, 58, 58, 1) 2%, rgba(49, 49, 116, 1) 36%, rgba(105, 0, 161, 1) 73%, rgba(166, 69, 252, 1) 100%)'
+        }}>
             <Toolbar>
                 <Grid container>
                     <Grid item xs={2}>
@@ -22,9 +23,9 @@ function Navbar({ links }) {
                     </Grid>
                     <Grid item xs={1} />
                     <Grid item xs={3}>
-                        <Box>
-                            <Button variant='contained'>Login</Button>
-                            <Button variant='contained'>SignUp</Button>
+                        <Box display="flex">
+                            <Button sx={{marginLeft: 'auto'}} variant='contained'>Login</Button>
+                            <Button sx={{marginLeft: 1}} variant='contained'>SignUp</Button>
                         </Box>
                     </Grid>
                 </Grid>

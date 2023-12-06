3e887e84e077e90188e6486497c08e731a9d933a
Divya R <kwrdivyaraj100@gmail.com>
2023-12-06 11:35:35 +0530

    Added AppBar with ShoppingCartCheckoutIcon and Tabs in Navbar component

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { AppBar, Grid, Toolbar, Typography } from '@mui/material';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <AppBar>
+            <Toolbar>
+                <Grid container>
+                    <Grid item xs={2}>
+                        <Typography>
+                            <ShoppingCartCheckoutIcon />
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <Tabs>
+                            
+                        </Tabs>
+
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
     )
 }
 

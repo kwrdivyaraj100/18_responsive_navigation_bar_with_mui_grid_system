54405f2a5776ba4bc5c7ed1cd44ca21203a30513
Divya R <kwrdivyaraj100@gmail.com>
2023-12-06 12:24:28 +0530

    Added Login and SignUp buttons to Grid component

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { AppBar, Grid, Tab, Tabs, Toolbar, Typography } from '@mui/material';
-import ShoppingCartCheckout from '@mui/icons-material/ShoppingCartCheckout'; 
+import { AppBar, Box, Button, Grid, Tab, Tabs, Toolbar, Typography } from '@mui/material';
+import ShoppingCartCheckout from '@mui/icons-material/ShoppingCartCheckout';
 
 function Navbar({ links }) {
-    const [value, setValue] = useState(0); 
+    const [value, setValue] = useState(0);
     return (
         <AppBar>
             <Toolbar>
@@ -13,12 +13,19 @@ function Navbar({ links }) {
                             <ShoppingCartCheckout />
                         </Typography>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={6}>
                         <Tabs indicatorColor="secondary" textColor='inherit' value={value} onChange={(e, val) => setValue(val)}>
                             {links.map((link, index) => (
                                 <Tab key={index} label={link} />
                             ))}
                         </Tabs>
+                    </Grid>
+                    <Grid item xs={1} />
+                    <Grid item xs={3}>
+                        <Box>
+                            <Button variant='contained'>Login</Button>
+                            <Button variant='contained'>SignUp</Button>
+                        </Box>
                     </Grid>
                 </Grid>
             </Toolbar>
